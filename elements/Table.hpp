d1af5d984f954581071655955ebbfa092ae71b2c
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-10 00:08:00 +0800

    Added methods: show, getCheckers

--- elements/Table.hpp
+++ elements/Table.hpp
@@ -1,9 +1,32 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "Checker.hpp"
+#include "IShow.hpp"
+
 /**
  * Доска для игры в шашки
  * 
  * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
  **/
-class Table
+class Table: public IShow
 {
-
+private:
+    /**
+     * Текстура доски
+     **/
+    sf::Texture tableTexture;
+    /**
+     * Спрайт доски
+     **/
+    sf::Sprite tableSprite;
+    /**
+     * Шашки
+     **/
+    std::vector<Checker> checkers;
+public:
+    sf::Drawable& show() override;
+    std::vector<Checker>& getCheckers();
 };
