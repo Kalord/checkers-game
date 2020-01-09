3d63d013197bab7e60c414b06f2ba71257d744a4
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-10 00:10:03 +0800

    Added methods: constructor, getName, getScope, incrementScope, toggleMove, canMove

--- players/Player.hpp
+++ players/Player.hpp
@@ -1,3 +1,8 @@
+#pragma once
+
+#include <string>
+#include <sys/types.h>
+
 /**
  * Базовый класс для игроков
  * 
@@ -5,5 +10,28 @@
  **/
 class Player
 {
+private:
+    /**
+     * Имя игрока
+     **/
+    std::string name;
+    /**
+     * Счет игрока
+     **/
+    u_int8_t scope;
+    /**
+     * Триггер указывающий на то, 
+     * что пользователь может перемещать шашки
+     **/
+    bool move;
+public:
+    Player(std::string name);
 
+    std::string getName();
+    u_int8_t getScope();
+
+    void incrementScope();
+
+    void toggleMove();
+    bool canMove();
 };
