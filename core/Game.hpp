61976875cef7e21664115bdb96e7572613818751
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-09 20:18:05 +0800

    Added Game.hpp

---
+++ core/Game.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../elements/Table.hpp"
+#include "../players/Player.hpp"
+
+/**
+ * Сущность игры
+ * 
+ * @author Artem Tyutnev <artem.tyutnev.developer@gmail.com>
+ **/
+class Game
+{
+private:
+    /**
+     * Доска для игры в шашки
+     **/
+    Table* table;
+
+    /**
+     * Игрок
+     **/
+    Player* player;
+
+    /**
+     * Оппонент для игры в шашки
+     **/
+    Player* opponent;
+
+public:
+    Game(Table* table, Player* player, Player* opponent);
+    ~Game();
+    /**
+     * Запуск игры
+     **/
+    void run();
+};
