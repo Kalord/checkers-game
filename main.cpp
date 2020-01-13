4803e0a290fd620943d1e1674ebafd6f80ebb874
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-13 21:31:26 +0800

    Update entry point

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 #include "players/Player.hpp"
 #include "core/Game.hpp"
 
+#define WIDTH 800
+#define HEIGHT 600
+
 int main()
 {
-    Table table("resources/table.png");
+    Table table("resources/table.png", WIDTH, HEIGHT);
 
     Player one(1, "John");
     Player two(2, "Mike");
@@ -15,15 +18,16 @@ int main()
     std::string whiteTexture = "resources/white.png";
     std::string blackTexture = "resources/black.png";
     table.initCheckers(
-        12, 
+        18, 
         one.getId(),
         two.getId(),
         whiteTexture,
         blackTexture
     );
 
+
     Game game(&table, &one, &two);
-    game.run();
+    game.run(WIDTH, HEIGHT);
 
     return 0;
 }
